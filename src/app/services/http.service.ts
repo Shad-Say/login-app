6e3e30ebb09396c1ff8d781288af6ffa239d094d
= <=>
2022-07-04 08:37:17 +0530

    push

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,10 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
   constructor(private http: HttpClient) {}
+
+  private baseUrl = 'https://forms-47.herokuapp.com/';
+
+  httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  registerUser(userRegisterData:any[]): Observable<any> {
+    return this.http.post(
+      this.baseUrl + 'user/register',
+      JSON.stringify(userRegisterData),
+      this.httpHeader
+    );
+
+}
 }
--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-signup-page',
